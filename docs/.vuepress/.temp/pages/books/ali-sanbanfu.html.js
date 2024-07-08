ba336085936f2da94bac80070b5ab6dc16636568
chenzongqiang <chenzongqiang@administratordeMac-mini.local>
2024-07-08 22:16:21 +0800

    docs: 三板斧

---
+++ docs/.vuepress/.temp/pages/books/ali-sanbanfu.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-2fa5792e\",\"path\":\"/books/ali-sanbanfu.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"阿里巴巴管理三板斧总结\",\"slug\":\"阿里巴巴管理三板斧总结\",\"link\":\"#阿里巴巴管理三板斧总结\",\"children\":[{\"level\":3,\"title\":\"管理三板斧的核心内容\",\"slug\":\"管理三板斧的核心内容\",\"link\":\"#管理三板斧的核心内容\",\"children\":[]},{\"level\":3,\"title\":\"招人\",\"slug\":\"招人\",\"link\":\"#招人\",\"children\":[]},{\"level\":3,\"title\":\"领导力修炼方面的三个核心方法:\",\"slug\":\"领导力修炼方面的三个核心方法\",\"link\":\"#领导力修炼方面的三个核心方法\",\"children\":[]},{\"level\":3,\"title\":\"工具图\",\"slug\":\"工具图\",\"link\":\"#工具图\",\"children\":[]}]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"books/ali-sanbanfu.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
