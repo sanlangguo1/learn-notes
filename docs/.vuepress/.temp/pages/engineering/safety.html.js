46378d02c58255ff079bceb30d6787d31c299209
三郎过 <huaidou11607@jushuitan.com>
2025-10-17 16:51:37 +0800

    docs: http

---
+++ docs/.vuepress/.temp/pages/engineering/safety.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-1aab8132\",\"path\":\"/engineering/safety.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"前端安全\",\"slug\":\"前端安全\",\"link\":\"#前端安全\",\"children\":[]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"engineering/safety.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ docs/.vuepress/.temp/pages/engineering/safety.html.vue
@@ -0,0 +1,8 @@
+<template><div><h3 id="前端安全" tabindex="-1"><a class="header-anchor" href="#前端安全" aria-hidden="true">#</a> 前端安全</h3>
+<h4 id="跨站脚本攻击-xss" tabindex="-1"><a class="header-anchor" href="#跨站脚本攻击-xss" aria-hidden="true">#</a> 跨站脚本攻击（XSS）</h4>
+<p><img src="/img/xss.png" alt="整个项目目录结构"></p>
+<h4 id="跨站请求伪造-csrf" tabindex="-1"><a class="header-anchor" href="#跨站请求伪造-csrf" aria-hidden="true">#</a> 跨站请求伪造（CSRF）</h4>
+<p><img src="/img/csrf&amp;ssrf.png" alt="整个项目目录结构"></p>
+</div></template>
+
+
